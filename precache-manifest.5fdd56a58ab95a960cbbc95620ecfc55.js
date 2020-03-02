798912c66898de9e153905a47f5a0c8c22cdcf05
Adrian <eidiazcas@gmail.com>
2020-03-02 00:00:10 -0500

    Updates

---
+++ precache-manifest.5fdd56a58ab95a960cbbc95620ecfc55.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51068a140f091c484b1c6a76d7a2f12f",
+    "url": "/profile_page/index.html"
+  },
+  {
+    "revision": "9c71f2a644cf43da2c40",
+    "url": "/profile_page/static/css/main.542a60c1.chunk.css"
+  },
+  {
+    "revision": "f1d0b8a1fe780cb71832",
+    "url": "/profile_page/static/js/2.a1ad310e.chunk.js"
+  },
+  {
+    "revision": "9c71f2a644cf43da2c40",
+    "url": "/profile_page/static/js/main.4369fece.chunk.js"
+  },
+  {
+    "revision": "af566017fba4e86ec36f",
+    "url": "/profile_page/static/js/runtime-main.208dad69.js"
+  },
+  {
+    "revision": "d7de6f9fb1828a6f8ec74dfbc4837618",
+    "url": "/profile_page/static/media/Layout_butterflies_1.d7de6f9f.png"
+  },
+  {
+    "revision": "f3c9af2efafde18ce93b8882eb1fce2b",
+    "url": "/profile_page/static/media/Layout_butterflies_2.f3c9af2e.png"
+  },
+  {
+    "revision": "4c01950b53d31e39db4e48682f9cedfd",
+    "url": "/profile_page/static/media/Layout_butterflies_3.4c01950b.png"
+  },
+  {
+    "revision": "506eb8dbf84358d051964d1a9b871d78",
+    "url": "/profile_page/static/media/Layout_butterflies_4.506eb8db.png"
+  },
+  {
+    "revision": "5256541268258e91b31b2378f92ec9b9",
+    "url": "/profile_page/static/media/Layout_butterflies_5.52565412.png"
+  },
+  {
+    "revision": "47fef8b4224d56d12fc1f0127409efce",
+    "url": "/profile_page/static/media/Layout_butterflies_6.47fef8b4.png"
+  },
+  {
+    "revision": "0b21acb9048f2e575440546eb972b208",
+    "url": "/profile_page/static/media/Layout_butterflies_bg.0b21acb9.png"
+  },
+  {
+    "revision": "9634ac873d81523e932be95bc0af530a",
+    "url": "/profile_page/static/media/Layout_butterflies_bulb_off.9634ac87.png"
+  },
+  {
+    "revision": "ce2c3668da51b0b449357385ea86fac6",
+    "url": "/profile_page/static/media/bootstrap.ce2c3668.png"
+  },
+  {
+    "revision": "27406b927accab33c1564a51160c1f95",
+    "url": "/profile_page/static/media/d3js.27406b92.png"
+  },
+  {
+    "revision": "aa9362ab753c56b5a80a413ceea6395d",
+    "url": "/profile_page/static/media/down_arrow.aa9362ab.svg"
+  },
+  {
+    "revision": "522cd947786223c980335caf0dd2a849",
+    "url": "/profile_page/static/media/eng.522cd947.png"
+  },
+  {
+    "revision": "02a55436000b175dbb0eb3e89022dec5",
+    "url": "/profile_page/static/media/ger.02a55436.png"
+  },
+  {
+    "revision": "b8b318056ffe26e520dcc999e8e1b0a3",
+    "url": "/profile_page/static/media/laravel.b8b31805.svg"
+  },
+  {
+    "revision": "6a56ba0405d5a6cda3dd2caa2f8c48f7",
+    "url": "/profile_page/static/media/left_arrow.6a56ba04.svg"
+  },
+  {
+    "revision": "6b6b08a563b050ddd9317b1d8dee4003",
+    "url": "/profile_page/static/media/letter.6b6b08a5.png"
+  },
+  {
+    "revision": "17e2765d54400474d3ce164c4fb298a0",
+    "url": "/profile_page/static/media/linux_windows.17e2765d.png"
+  },
+  {
+    "revision": "d2b1e19091a3530dfb8bec06b5a926dc",
+    "url": "/profile_page/static/media/materialui.d2b1e190.png"
+  },
+  {
+    "revision": "69a09ead24b43ba1a36846f8ea2b7e9a",
+    "url": "/profile_page/static/media/node.69a09ead.svg"
+  },
+  {
+    "revision": "74e4489face12459112868ebe3d7b575",
+    "url": "/profile_page/static/media/pensativo.74e4489f.png"
+  },
+  {
+    "revision": "03c4e1f60d0bd3602c8fe0fac0c1bdf0",
+    "url": "/profile_page/static/media/postgresql.03c4e1f6.png"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/profile_page/static/media/react.05a3e30f.svg"
+  },
+  {
+    "revision": "d89aa96ee37364816f7ba286343840ea",
+    "url": "/profile_page/static/media/redux.d89aa96e.png"
+  },
+  {
+    "revision": "440d6ab028bd3bacae839fa1ede73b12",
+    "url": "/profile_page/static/media/right_arrow.440d6ab0.svg"
+  },
+  {
+    "revision": "95852bcf4d945cfdc9a5f2cf61d222c6",
+    "url": "/profile_page/static/media/up_arrow.95852bcf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.81012af72b76d3593a6339d884ed062e.js"
+  "/profile_page/precache-manifest.5fdd56a58ab95a960cbbc95620ecfc55.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/profile_page/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
