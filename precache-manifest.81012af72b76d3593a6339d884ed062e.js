79ca7eb4c9825e4583cf2c224217d932bad1af43
Adrian <eidiazcas@gmail.com>
2020-03-01 23:51:50 -0500

    Updates

---
+++ precache-manifest.81012af72b76d3593a6339d884ed062e.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e2f4b8238ed6675b0926d1539e240fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "12c08e88ca826469adb1",
+    "url": "/static/css/main.542a60c1.chunk.css"
+  },
+  {
+    "revision": "f1d0b8a1fe780cb71832",
+    "url": "/static/js/2.a1ad310e.chunk.js"
+  },
+  {
+    "revision": "12c08e88ca826469adb1",
+    "url": "/static/js/main.03d34c2a.chunk.js"
+  },
+  {
+    "revision": "b8bc94dab961d0da1d14",
+    "url": "/static/js/runtime-main.51d7cef1.js"
+  },
+  {
+    "revision": "d7de6f9fb1828a6f8ec74dfbc4837618",
+    "url": "/static/media/Layout_butterflies_1.d7de6f9f.png"
+  },
+  {
+    "revision": "f3c9af2efafde18ce93b8882eb1fce2b",
+    "url": "/static/media/Layout_butterflies_2.f3c9af2e.png"
+  },
+  {
+    "revision": "4c01950b53d31e39db4e48682f9cedfd",
+    "url": "/static/media/Layout_butterflies_3.4c01950b.png"
+  },
+  {
+    "revision": "506eb8dbf84358d051964d1a9b871d78",
+    "url": "/static/media/Layout_butterflies_4.506eb8db.png"
+  },
+  {
+    "revision": "5256541268258e91b31b2378f92ec9b9",
+    "url": "/static/media/Layout_butterflies_5.52565412.png"
+  },
+  {
+    "revision": "47fef8b4224d56d12fc1f0127409efce",
+    "url": "/static/media/Layout_butterflies_6.47fef8b4.png"
+  },
+  {
+    "revision": "0b21acb9048f2e575440546eb972b208",
+    "url": "/static/media/Layout_butterflies_bg.0b21acb9.png"
+  },
+  {
+    "revision": "9634ac873d81523e932be95bc0af530a",
+    "url": "/static/media/Layout_butterflies_bulb_off.9634ac87.png"
+  },
+  {
+    "revision": "ce2c3668da51b0b449357385ea86fac6",
+    "url": "/static/media/bootstrap.ce2c3668.png"
+  },
+  {
+    "revision": "27406b927accab33c1564a51160c1f95",
+    "url": "/static/media/d3js.27406b92.png"
+  },
+  {
+    "revision": "aa9362ab753c56b5a80a413ceea6395d",
+    "url": "/static/media/down_arrow.aa9362ab.svg"
+  },
+  {
+    "revision": "522cd947786223c980335caf0dd2a849",
+    "url": "/static/media/eng.522cd947.png"
+  },
+  {
+    "revision": "02a55436000b175dbb0eb3e89022dec5",
+    "url": "/static/media/ger.02a55436.png"
+  },
+  {
+    "revision": "b8b318056ffe26e520dcc999e8e1b0a3",
+    "url": "/static/media/laravel.b8b31805.svg"
+  },
+  {
+    "revision": "6a56ba0405d5a6cda3dd2caa2f8c48f7",
+    "url": "/static/media/left_arrow.6a56ba04.svg"
+  },
+  {
+    "revision": "6b6b08a563b050ddd9317b1d8dee4003",
+    "url": "/static/media/letter.6b6b08a5.png"
+  },
+  {
+    "revision": "17e2765d54400474d3ce164c4fb298a0",
+    "url": "/static/media/linux_windows.17e2765d.png"
+  },
+  {
+    "revision": "d2b1e19091a3530dfb8bec06b5a926dc",
+    "url": "/static/media/materialui.d2b1e190.png"
+  },
+  {
+    "revision": "69a09ead24b43ba1a36846f8ea2b7e9a",
+    "url": "/static/media/node.69a09ead.svg"
+  },
+  {
+    "revision": "74e4489face12459112868ebe3d7b575",
+    "url": "/static/media/pensativo.74e4489f.png"
+  },
+  {
+    "revision": "03c4e1f60d0bd3602c8fe0fac0c1bdf0",
+    "url": "/static/media/postgresql.03c4e1f6.png"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/static/media/react.05a3e30f.svg"
+  },
+  {
+    "revision": "d89aa96ee37364816f7ba286343840ea",
+    "url": "/static/media/redux.d89aa96e.png"
+  },
+  {
+    "revision": "440d6ab028bd3bacae839fa1ede73b12",
+    "url": "/static/media/right_arrow.440d6ab0.svg"
+  },
+  {
+    "revision": "95852bcf4d945cfdc9a5f2cf61d222c6",
+    "url": "/static/media/up_arrow.95852bcf.svg"
+  }
+]);
