84e79be4dfebba52a346e8bc0be2a6d2ab251aa4
Adrian <eidiazcas@gmail.com>
2020-06-15 16:00:38 -0500

    description grammar fix

--- src/components/description/description_texts.js
+++ src/components/description/description_texts.js
@@ -1,8 +1,8 @@
 export const description_text = {
-    eng: "I'm a a clean coder and a very fast learner, test me!!. \
+    eng: "I'm a clean coder and a very fast learner, test me!!. \
 I think high income is good but I'm more motivated by memorable projects \
 and challenging problems.",
-    spa: "Soy muy rapido aprendiendo y un codificador limpio, ponme a prueba!!. \
+    spa: "Soy muy rapido aprendiendo y un programador con buenas prácticas, ponme a prueba!!. \
 Creo que los ingresos altos son buenos, pero me motivan más los proyectos memorables \
 y problemas desafiantes.",
     ger: "Ich lerne schnell und bin ein sauberer Programmierer. Teste mich!!. \
